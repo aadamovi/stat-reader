2610bef6f25523333a6f146d130ba0120520647f
aadamovi <artua85@gmail.com>
2020-05-19 14:08:04 +0100

    Initial empty appp commit

---
+++ src/main/java/com/movich/stat/reader/StatReaderApplication.java
@@ -0,0 +1,13 @@
+package com.movich.stat.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movich/stat/reader/StatReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movich.stat.reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
